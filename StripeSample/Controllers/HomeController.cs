a8dff9a999f7a807e05493deb39964772f6ebd1a
Christos Matskas <chris@towzietyke.co.uk>
2014-09-03 13:56:26 +0100

    A Stripe Sample project
    Full solution added for blog purposes

---
+++ StripeSample/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System.Web.Mvc;
+
+namespace StripeSample.Controllers
+{
+    using System.Threading.Tasks;
+    using Models;
+    using Stripe;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Charge()
+        {
+            ViewBag.Message = "Learn how to process payments with Stripe";
+
+            return View(new StripeChargeModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Charge(StripeChargeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var chargeId = await ProcessPayment(model);
+            return View("Index");
+        }
+
+        private async Task<string> ProcessPayment(StripeChargeModel model)
+        {
+            return await Task.Run(() =>
+            {
+                var myCharge = new StripeChargeCreateOptions
+                {
+                    // convert the amount of £12.50 to pennies i.e. 1250
+                    Amount = (int)(model.Amount * 100),
+                    Currency = "gbp",
+                    Description = "Description for test charge",
+                    TokenId = model.Token
+                };
+
+                var chargeService = new StripeChargeService("sk_test_k1ooss8z5FpUBm4CZzpYUMqG");
+                var stripeCharge = chargeService.Create(myCharge);
+
+                return stripeCharge.Id;
+           });
+        }
+    }
+}
---
+++ StripeSample/Models/StripeChargeModel.cs
@@ -0,0 +1,21 @@
+﻿namespace StripeSample.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class StripeChargeModel
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+        // These fields are optional and are not 
+        // required for the creation of the token
+        public string CardHolderName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressCity { get; set; }
+        public string AddressPostcode { get; set; }
+        public string AddressCountry { get; set; }
+    }
+}
